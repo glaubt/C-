4a1bee44555ee59c6e6e34af0b88e1bea4dddc06
Arturs Vents <161161836+glaubt@users.noreply.github.com>
2024-03-15 18:30:55 +0100

    Add files via upload

---
+++ Calculator.console/Calculator.console/Calculator.console.cpp
@@ -0,0 +1,44 @@
+﻿//Calculator
+#include <iostream>
+
+int main() {
+    char operation;
+    double num1, num2;
+
+    std::cout << "Enter the first number: ";
+    std::cin >> num1;
+
+    std::cout << "Enter the first symbol (+, -, *, /): ";
+    std::cin >> operation;
+
+    std::cout << "Enter the next number: ";
+    std::cin >> num2;
+
+    double result;
+    switch (operation) {
+    case '+':
+        result = num1 + num2;
+        break;
+    case '-':
+        result = num1 - num2;
+        break;
+    case '*':
+        result = num1 * num2;
+        break;
+    case '/':
+        if (num2 != 0)
+            result = num1 / num2;
+        else {
+            std::cerr << "ERROR!" << std::endl;
+            return 1;
+        }
+        break;
+    default:
+        std::cerr << "ERROR!" << std::endl;
+        return 1;
+    }
+
+    std::cout << "ANSWER: " << result << std::endl;
+
+    return 0;
+}
