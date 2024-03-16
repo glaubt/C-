3e5865505d1e2733e1b7bb0d30152da9db2e488c
Arturs Vents <161161836+glaubt@users.noreply.github.com>
2024-03-16 21:40:12 +0100

    Voltage

---
+++ Steckdose_Voltage/Steckdose/Steckdose.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <chrono>
+#include <thread>
+#include <random>
+
+int main() {
+    std::cout << "Checking the Voltage and Current:\n";
+
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<int> dis(224, 230);
+
+    while (true) {
+        int voltage = dis(gen);
+
+        std::cout << "Voltage is : " << voltage << " V\n";
+
+        std::this_thread::sleep_for(std::chrono::seconds(2));
+    }
+
+    return 0;
+}
