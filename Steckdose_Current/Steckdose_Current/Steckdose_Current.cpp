7d59796d8b30be2721b622ab3923fe8c0064bdeb
Arturs Vents <161161836+glaubt@users.noreply.github.com>
2024-03-16 21:47:47 +0100

    Current

---
+++ Steckdose_Current/Steckdose_Current/Steckdose_Current.cpp
@@ -0,0 +1,23 @@
+﻿//Steckdose (Current)
+#include <iostream>
+#include <chrono>
+#include <thread>
+#include <random>
+
+int main() {
+    std::cout << "Checking the Current:\n";
+
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<int> dis(3,4);
+
+    while (true) {
+        int current = dis(gen);
+
+        std::cout << "Current is : " << current << " A\n";
+
+        std::this_thread::sleep_for(std::chrono::seconds(2));
+    }
+
+    return 0;
+}
